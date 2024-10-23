80f7a5263b6aeaf118eb4635bac382735a42c977
Jessica Ju <67920778+jrdefideveloper@users.noreply.github.com>
2024-10-23 16:55:23 +0900

    font color changes

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,8 @@ const Home: React.FC = () => {
   const [image, setImage] = useState<string | null>(null);
   const canvasRef = useRef<HTMLDivElement>(null);
 
+  const backgroundImageUrl = 'https://dd.dexscreener.com/ds-data/tokens/solana/C65t4Bd52R1ZdV1GVzzSyLqphoPrShiajsK5nJBrpump/header.png?size=xl&key=c50d42';
+
   const handleImageUpload = (e: React.ChangeEvent<HTMLInputElement>) => {
     const file = e.target.files?.[0];
     if (file) {
@@ -42,8 +44,19 @@ const Home: React.FC = () => {
   };
 
   return (
-    <div>
+    <div  style={{
+      backgroundImage: `url(${backgroundImageUrl})`, // Set the background image for the webpage
+      backgroundSize: 'cover', // Cover the whole area
+      backgroundPosition: 'center',
+      minHeight: '100vh', // Ensure the div takes full height of the viewport
+      color: 'rgba(128, 0, 32, 1)', // Burgundy text color
+      padding: '20px', // Optional: Add some padding
+    }}
+    >
+      
+      <center><h1 style={{ fontSize: '2em', marginBottom: '20px' }}>Welcome to 888</h1></center>
       <h1>Upload Image and Add Watermark</h1>
+      
       <input type="file" accept="image/*" onChange={handleImageUpload} />
       {image && (
         <div>
@@ -53,7 +66,7 @@ const Home: React.FC = () => {
               style={{
                 position: 'absolute',
                 bottom: '10px',
-                right: '10px',
+                right: '50px',
                 color: 'rgba(255, 69, 0, 0.75)',  // Red-orange, 50% opacity
                 fontSize: '200px',
                 fontWeight: 'bold',
